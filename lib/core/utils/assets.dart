17ef9187b148c560552ee41deca9db0ab77d9308
hasanShaaban <hsahasan16@gmail.com>
2025-04-15 12:06:26 +0000

    setup the work space, the assets are imported,  the constant file and text style file have been implemented, note : the assets file is not compleated yet

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for iconsAccountCircle
+  /// assets/icons/account_circle.svg
+  static const String iconsAccountCircle = "assets/icons/account_circle.svg";
+
+  /// Assets for iconsArrowRightToBracket
+  /// assets/icons/arrow-right-to-bracket.svg
+  static const String iconsArrowRightToBracket = "assets/icons/arrow-right-to-bracket.svg";
+
+  /// Assets for iconsBastaLogo
+  /// assets/icons/basta_logo.png
+  static const String iconsBastaLogo = "assets/icons/basta_logo.png";
+
+  /// Assets for iconsEnvelope
+  /// assets/icons/envelope.svg
+  static const String iconsEnvelope = "assets/icons/envelope.svg";
+
+  /// Assets for iconsEsta
+  /// assets/icons/esta.svg
+  static const String iconsEsta = "assets/icons/esta.svg";
+
+  /// Assets for iconsGoogleIcon
+  /// assets/icons/google_icon.svg
+  static const String iconsGoogleIcon = "assets/icons/google_icon.svg";
+
+  /// Assets for iconsPassword
+  /// assets/icons/password.svg
+  static const String iconsPassword = "assets/icons/password.svg";
+
+  /// Assets for imagesOnBoarding1
+  /// assets/images/on_boarding1.json
+  static const String imagesOnBoarding1 = "assets/images/on_boarding1.json";
+
+  /// Assets for imagesOnBoarding2
+  /// assets/images/on_boarding2.json
+  static const String imagesOnBoarding2 = "assets/images/on_boarding2.json";
+
+  /// Assets for imagesOnBoarding3
+  /// assets/images/on_boarding3.json
+  static const String imagesOnBoarding3 = "assets/images/on_boarding3.json";
+
+  /// Assets for imagesPath
+  /// assets/images/Path.svg
+  static const String imagesPath = "assets/images/Path.svg";
+}
+
---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xFF353068);
+  static const Color secondryColor = Color(0xFF995CE3);
+  static const Color thirdColor = Color(0xFF00E7DD);
+  static const Color fourthColor = Color(0xFF9287FF);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color deviderColor = Color(0xFFC5C5C5);
+}
