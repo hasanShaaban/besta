86225e9b774c8d78e31fb89888227fd1f1906c09
hasanShaaban <hsahasan16@gmail.com>
2025-04-16 11:53:59 +0000

    add localization, and set up the application on arabic

--- lib/feature/on_boarding/presentation/widgets/on_boarding_view_body.dart
+++ lib/feature/on_boarding/presentation/widgets/on_boarding_view_body.dart
@@ -5,6 +5,11 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text('asdf')
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-
 import 'package:besta/core/functions/on_generat_route.dart';
 
 import 'package:besta/core/utils/constants.dart';
 import 'package:besta/feature/splash/presentation/splash_view.dart';
+import 'package:besta/generated/l10n.dart';
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,11 +16,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates:const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale:const Locale('ar'),
       theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.backgroundColor,
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
-        fontFamily: 'Cairo'
-      ),
+          scaffoldBackgroundColor: AppColors.backgroundColor,
+          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
+          fontFamily: 'Cairo'),
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
       initialRoute: SplashView.routeName,
